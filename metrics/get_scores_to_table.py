24fa5dfcbe6236e8f0095a83ec994ca3e8de5127
alextimans <36069099+alextimans@users.noreply.github.com>
2022-08-18 10:49:57 +0200

    fixes

--- metrics/get_scores_to_table.py
+++ metrics/get_scores_to_table.py
@@ -14,6 +14,12 @@ def create_parser() -> argparse.ArgumentParser:
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--test_pred_path", type=str, default=None, required=True,
                         help="Test pred path.")
+    parser.add_argument("--create_table", type=str, default="True", required=False, choices=["True", "False"],
+                        help="'Boolean' specifying if get_scores_to_table function should be called.")
+    parser.add_argument("--agg_table", type=str, default="False", required=False, choices=["True", "False"],
+                        help="'Boolean' specifying if aggregate_tables function should be called.")
+    parser.add_argument("--agg_nr", type=int, default=None, required=False,
+                        help="Nr. of tables to aggregate, used to cycle through folders.")
 
     return parser
 
@@ -72,10 +78,63 @@ def get_scores_to_table(test_pred_path: str):
     # s.split("/")[-1][:-4].split("_")
 
 
+def aggregate_tables(test_pred_path: str, agg_nr: int):
+
+    scorenames = get_score_names()
+    colnames = list(np.delete(scorenames[1:-1].split(", "), [4,5,6,7]))
+    colnames.insert(0, "uq_method")
+    colnames.insert(0, "city")
+
+    fnames = ["results_speed_mask", "results_speed", "results_vol_mask", "results_vol"]
+    folder = test_pred_path.split("/")[-2]
+
+    for f in fnames:
+        df = pd.DataFrame(columns=colnames)
+
+        for nr in range(1, agg_nr+1):
+            path = os.path.join(os.path.split(test_pred_path)[0], f"{folder}_{nr}", "scores", f + ".csv")
+            df = pd.concat([df, pd.read_csv(path)])
+
+        # split str columns and make numeric for means
+        df[["m_gt", "rm", "std_gt"]] = df["mean_gt"].str.split(" ", -1, expand=True)
+        df[["m_pred", "rm", "std_pred"]] = df["mean_pred"].str.split(" ", -1, expand=True)
+        df[["m_unc", "rm", "std_unc"]] = df["mean_unc"].str.split(" ", -1, expand=True)
+        df[["m_mse", "rm", "std_mse"]] = df["mean_mse"].str.split(" ", -1, expand=True)
+        df = df.drop(["rm", "mean_gt", "mean_pred", "mean_unc", "mean_mse"], axis=1)
+        df.iloc[:, 2:] = df.iloc[:, 2:].apply(pd.to_numeric)
+
+        # group by (city, uq_method) and take means over all columns
+        res = df.groupby(["city", "uq_method"]).mean().reset_index()
+        # res = res.round({"PI_width": 3, "cover": 3, "ENCE": 3, "CoV": 3,
+        #                  "corr": 3, "sp_corr": 3, "m_gt": 2, "std_gt": 2,
+        #                  "m_pred": 2, "std_pred": 2, "m_unc": 2, "std_unc": 2,
+        #                  "m_mse": 2, "std_mse": 2})
+        res = res.round(3)
+
+        # sort uq methods by desired order
+        sorter = ["point", "combo", "ensemble", "bnorm", "tta", "patches"]
+        res["uq_method"] = res["uq_method"].astype("category").cat.set_categories(sorter)
+        res = res.sort_values(["city", "uq_method"]).groupby("city").head(6)
+
+        # create new str columns
+        s = " +- "
+        res.insert(2, "mean_gt", res["m_gt"].astype(str) + s + res["std_gt"].astype(str))
+        res.insert(3, "mean_pred", res["m_pred"].astype(str) + s + res["std_pred"].astype(str))
+        res.insert(4, "mean_unc", res["m_unc"].astype(str) + s + res["std_unc"].astype(str))
+        res.insert(5, "mean_mse", res["m_mse"].astype(str) + s + res["std_mse"].astype(str))
+        res = res.drop(["m_gt", "std_gt", "m_pred", "std_pred", "m_unc", "std_unc", "m_mse", "std_mse"], axis=1)
+
+        dfname = f"{f}_mean_{agg_nr}.csv"
+        res.to_csv(os.path.join(os.path.split(test_pred_path)[0], dfname), index=False, na_rep='N/A')
+
+
 def main():
     parser = create_parser()
     args = parser.parse_args()
-    get_scores_to_table(args.test_pred_path)
+    if eval(args.create_table) is not False:
+        get_scores_to_table(args.test_pred_path)
+    if eval(args.agg_table) is not False:
+        aggregate_tables(args.test_pred_path, args.agg_nr)
 
 
 if __name__ == "__main__":
